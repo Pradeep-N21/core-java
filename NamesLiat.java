40cde06a0442ad97e2478332035e98587e40a5c5
Pradeep-N21 <pradeepn.xworkz@gmail.com>
2023-10-04 19:30:35 +0530

    String Task

---
+++ NamesLiat.java
@@ -0,0 +1,41 @@
+public class NamesList
+{
+  public static void main(String[] args)
+  {
+	  String name1 = "Rahul";
+	  String name2 = "Ram";
+	  String name3 = "Rahim";
+	  String name4 = "Rakesh";
+	  String name5 = "Ramesh";
+	  String name6 = "Adarsh";
+	  String name7 = "Ram";
+	  String name8 = "Prathap";
+	  String name9 = "Praveen";
+	  String name10 = "Adarsh";
+	  
+	  String str1 = new String("Rakshitha");
+	  String str2 = new String("Praveen");
+	  String str3 = new String("Raju");
+	  String str4 = new String("Darshan");
+	  String str5 = new String("Jeevan");
+      String str6 = new String("Deepak");
+      String str7 = new String("Shashank");
+      String str8 = new String("Deepak");
+      String str9 = new String("Ramesh");
+      String str10 = new String("Pooja");
+
+      name1 = "Prajwal";
+      name10 = "Rakesh";
+      name3 = "Suraj";
+      name4 = "Rahul";
+      name8 = "Pavan";
+
+  System.out.println( name2 == name7 );
+  System.out.println( name1 == name4 );
+  System.out.println( name9 == str2 );
+  System.out.println( name5 == str9 );
+  System.out.println( name8 == str6 );
+  
+  }
+}  
+	  
