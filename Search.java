f1fd16cb59dfb21db6593434633bbf69064ec766
Pradeep-N21 <pradeepn.xworkz@gmail.com>
2023-10-06 11:45:56 +0530

    60 String Variables

---
+++ Search.java
@@ -0,0 +1,98 @@
+public class Search
+{
+	public static void main(String[] args)
+	{
+
+
+		String countryName = "United States";
+		String capitalCity = "Washington, D.C.";
+		String population = "331 million";
+		String currency = "US Dollar (USD)";
+		String officialLanguage = "English";
+		String nationalAnthem = "The Star-Spangled Banner";
+		String landmark = "Statue of Liberty";
+		String famousFood = "Hamburgers";
+		String timeZone = "Eastern Standard Time (EST)";
+		String president = "Donald Trump";
+		String countryCode = "+02";
+		String countryMotto = "In God We Trust";
+		String nationalBird = "Bald Eagle";
+		String climate = "Diverse, from tropical to arctic";
+		String geography = "Varied landscapes";
+		String flagColors = "Red, White, and Blue";
+		String nationalSport = "American Football";
+		String famousRiver = "Mississippi River";
+		String stateCount = "49 states";
+		String largestCity = "New York City";
+		String nationalHoliday = "Independence Day (July 4th)";
+		String popularSingers = "Beyoncé, Taylor Swift";
+		String stateNickname = "The Sunshine State (Florida)";
+		String stateFlower = "Rose";
+		String nationalPark = "Yellowstone National Park";
+		String stateMotto = "Live Free or Die (New Hampshire)";
+		String stateBird = "Northern Cardinal (Illinois)";
+		String stateDance = "Square Dance (Oklahoma)";
+		String stateFruit = "Apple";
+		String stateTree = "Pecan Tree";
+		String famousCity = "New York City";
+		String famousFlower = "Georgia";
+		
+		
+		president = "Joe Biden";
+		stateCount = "50 States";
+		countryCode = "+1";
+		stateFlower = "Georgia";
+		stateTree = "Texas";
+		
+		
+		
+		String productName = new String("Laptop");
+		String actorName = new String("Brad Pitt");
+		String softwareCategory = new String("Graphics Design");
+		String footballTeam = new String("Manchester United");
+		String buildingMaterial = new String("Brick");
+		String artMedium = new String("Oil Paint");
+		String beverageType = new String("Tea");
+		String instrumentBrand = new String("Fender");
+		String languageName = new String("Spanish");
+		String mountainName = new String("Mount Everest");
+		String vehicleType = new String("Motorcycle");
+		String bookAuthor = new String("J.K. Rowling");
+		String companyIndustry = new String("Tech");
+		String mealType = new String("Breakfast");
+		String gameGenre = new String("RPG");
+		String dogBreed = new String("Golden Retriever");
+		String computerOS = new String("Windows");
+		String movieDirector = new String("Christopher Nolan");
+		String paintingStyle = new String("Impressionism");
+		String shoeBrand = new String("Nike");
+		String weatherCondition = new String("Sunny");
+		String spaceObject = new String("Saturn");
+		String fruitVariety = new String("Granny Smith Apple");
+		String bookPublisher = new String("Penguin Books");
+		String currencyName = new String("Euro");
+		String architecturalStyle = new String("Gothic");
+		String coffeeRoast = new String("Dark Roast");
+		String fishSpecies = new String("Salmon");
+		String phoneBrand = new String("Samsung");
+		String filmStudio = new String("Paramount Pictures");
+		
+		
+		footballTeam = "Bangalore FC";
+		shoeBrand = "Adidas";
+		languageName = "English";
+		weatherCondition = "Rainy";
+		phoneBrand = "Apple";
+		
+		
+		System.out.println(largestCity == famousCity);
+		System.out.println(nationalSport == footballTeam);
+		System.out.println(phoneBrand == stateFruit);
+		System.out.println(languageName == officialLanguage);
+		System.out.println(famousFlower == stateFlower);
+		
+	}
+}
+		
+		
+
